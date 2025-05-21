984e8720bb3a96da62234bf92cd54f7c510c5083
Syed Ameen <syedameenabrar786@gmail.com>
2025-05-21 15:53:31 +0530

    download file package installation

--- projects/questionnaire-player/src/lib/services/api.service.ts
+++ projects/questionnaire-player/src/lib/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import * as urlConfig from './../constants/url-config.json';
-
+import { saveAs } from 'file-saver';
 @Injectable({
   providedIn: 'root'
 })
@@ -61,5 +61,14 @@ export class ApiService {
         console.error('Error retrieving access token:', error);
         return null;
     }
+    }
+
+    downloadFile(url: string) {
+      this.http.get(url, { responseType: 'blob' })
+        .subscribe(blob => {
+          saveAs(blob, `observation_report${Date.now()}`);
+        }, error => {
+          console.error('Error downloading file:', error);
+      });
     }
 }
